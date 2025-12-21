82da88905f8c17e15e31b0ed3902ebc1b63ae5d2
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2025-12-21 16:41:02 +0200

    inserting pom.xm

---
+++ src/main/java/com/ngabonzizacedrick/user_tracker_service/UserTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ngabonzizacedrick.user_tracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngabonzizacedrick/user_tracker_service/UserTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngabonzizacedrick.user_tracker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
